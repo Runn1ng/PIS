c420c2e81aea02f89b5dde0e94d1042901a193b6
Timofei <tuistssip@gmail.com>
2020-10-07 23:25:17 +0500

    reformat

--- PIS/Services/Auth.cs
+++ PIS/Services/Auth.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PIS.Services
 {
-    class Auth
+    internal class Auth
     {
-        private void SignIn()
+        public static void SignIn()
         {
             throw new NotImplementedException();
         }
