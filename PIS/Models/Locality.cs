2d729e0ed6d9a30bcd6668a11a9fe19e20f70a95
Archie <tuistssip@gmail.com>
2020-12-23 19:39:34 +0500

    update db connection

--- PIS/Models/Locality.cs
+++ PIS/Models/Locality.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,6 @@ namespace PIS.Models
     public class Locality
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        [Column(TypeName = "nvarchar")] public string Name { get; set; }
     }
 }
